705fbeb58e808dcfa93096f4ac4a846fc16e2278
Tran Bao Toan <Maybee@Hackintoshs-Mac.local>
2017-03-27 08:36:10 +0700

    checkbox

---
+++ mFIR/Checkbox.h
@@ -0,0 +1,16 @@
+//
+//  Checkbox.h
+//  mFIR
+//
+//  Created by Tran Bao Toan on 3/24/17.
+//  Copyright © 2017 poscantho. All rights reserved.
+//
+
+#ifndef Checkbox_h
+#define Checkbox_h
+
+
+#endif /* Checkbox_h */
+
+
+#import "BEMCheckBox.h"
